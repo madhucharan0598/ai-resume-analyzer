ec77300cb435ab06f5a78192d184870fbbe90c4e
Madhucharan Durgam <durgammadhucharan.131650@marwadiuniversity.ac.in>
2026-04-12 21:22:56 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,20 +1,17 @@
 const jwt = require("jsonwebtoken")
-
-module.exports = (req, res, next) => {
-
+module.exports = function (req, res, next) {
   const authHeader = req.headers.authorization
-
   if (!authHeader) {
     return res.status(401).json({ message: "No token provided" })
   }
-
-  const token = authHeader.split(" ")[1]
-
   try {
+    const token = authHeader.split(" ")[1]
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = decoded
+    req.user = {
+      id: decoded.id
+    }
     next()
-  } catch (err) {
+  } catch (error) {
     return res.status(401).json({ message: "Invalid token" })
   }
 }
