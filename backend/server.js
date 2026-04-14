07efd521c8eee5edcadb47ca397a7b263829d7d0
Madhucharan Durgam <durgammadhucharan.131650@marwadiuniversity.ac.in>
2026-04-14 18:32:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,35 +7,13 @@ const authRoutes = require("./routes/authRoutes")
 const resumeRoutes = require("./routes/resumeRoutes")
 
 const app = express()
-
-/* Connect DB */
 connectDB()
 
-/* Middleware */
-app.use(cors({
-  origin: "*"
-}))
+app.use(cors({ origin: "*" }))
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/resume", resumeRoutes)
 
-/* Test Route */
-app.get("/", (req, res) => {
-  res.send("API Running 🚀")
-})
-
-/* Server Start */
+app.get("/", (req, res) => res.send("API Running"))
 const PORT = process.env.PORT || 5000
-
-/* GLOBAL ERROR HANDLER */
-app.use((err, req, res, next) => {
-  console.error(err.message)
-
-  res.status(500).json({
-    message: err.message || "Server Error"
-  })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
